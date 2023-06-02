7725a7161e2a6926e293d47abbe2073d4f6b68b0
santhoshmlops <santhoshmlops@gmail.com>
2023-06-02 21:39:49 +0530

    Data_ingestion completed

---
+++ main.py
@@ -0,0 +1,14 @@
+from textsummarizer.pipeline.train_pipeline import DataIngestionTrainingPipeline
+
+from textsummarizer.logger import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
