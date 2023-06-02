0540f0571a3b0a5455cf0f4b367aac922ff5907e
santhoshmlops <santhoshmlops@gmail.com>
2023-06-02 21:54:44 +0530

    Data_validation

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from textsummarizer.pipeline.train_pipeline import DataIngestionTrainingPipeline
-
+from textsummarizer.pipeline.train_pipeline import DataTrainingPipeline
 from textsummarizer.logger import logger
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
+   data_training = DataTrainingPipeline()
+   data_training.dataingestiontrainingpipeline()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_training = DataTrainingPipeline()
+   data_training.datavalidationtrainingpipeline()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/textsummarizer/pipeline/train_pipeline.py
+++ src/textsummarizer/pipeline/train_pipeline.py
@@ -1,15 +1,23 @@
 from textsummarizer.config.configuration import ConfigurationManager
 from textsummarizer.components.data_ingestion import DataIngestion
+from textsummarizer.components.data_validation import DataValiadtion
 from textsummarizer.logger import logger
 
 
-class DataIngestionTrainingPipeline:
+class DataTrainingPipeline:
     def __init__(self):
         pass
 
-    def main(self):
+    def dataingestiontrainingpipeline(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+
+
+    def datavalidationtrainingpipeline(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
